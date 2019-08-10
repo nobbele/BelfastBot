355fbee6e82bb81a5be14be677950fa7ffceb0d8
TheJayDuck <therealjayduck@gmail.com>
2019-08-10 20:44:16 +0300

    Adds welcome message

--- SenkoSanBot/SenkoSan.cs
+++ SenkoSanBot/SenkoSan.cs
@@ -52,6 +52,11 @@ namespace SenkoSanBot
 
 
                 client.Log += LogMessageAsync;
+                client.UserJoined += async (SocketGuildUser user) => 
+                {
+                    await user.Guild.SystemChannel.SendMessageAsync(string.Format(config.Configuration.WelcomeMessage, user.Mention));
+                };
+
                 services.GetRequiredService<CommandService>().Log += LogMessageAsync;
 
                 await client.LoginAsync(TokenType.Bot, config.Configuration.Token, true);
