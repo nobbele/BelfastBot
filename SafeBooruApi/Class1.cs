27fe01a3bab5337fa12aada3f0a26dda66447fba
TheJayDuck <therealjayduck@gmail.com>
2019-08-10 15:38:15 +0300

    Adds unfinished safebooru api.

---
+++ SafeBooruApi/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SafeBooruApi
+{
+    public class Class1
+    {
+        public static readonly string BaseUrl = "https://safebooru.org/index.php?page=dapi&s=post&q=index&tags=";
+
+        public async Task GetBooruImage(string tag)
+        {
+
+            using (HttpClient client = new HttpClient())
+            {
+                Stream fileStream = await client.GetStreamAsync($"{BaseUrl}&tags={tag}");
+                XDocument xdoc = await XDocument.LoadAsync(fileStream, LoadOptions.None, CancellationToken.None);
+                
+            }
+
+
+        }
+
+    }
+}
