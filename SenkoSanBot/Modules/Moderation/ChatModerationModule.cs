a462302025fbee72a753758fea1d15e1fcdb63bd
TheJayDuck <therealjayduck@gmail.com>
2019-08-10 21:36:50 +0300

    Adds ChatModeration Module

---
+++ SenkoSanBot/Modules/Moderation/ChatModerationModule.cs
@@ -0,0 +1,32 @@
+﻿using Discord;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace SenkoSanBot.Modules.Moderation
+{
+    public class ChatModerationModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("purge")]
+        [RequireUserPermission(GuildPermission.ManageMessages)]
+        [RequireBotPermission(GuildPermission.ManageMessages)]
+        public async Task PurgeModule(int amount = 1)
+        {
+            if (amount > 100)
+            {
+                var botMessage_1 = await Context.Channel.SendMessageAsync(":x: You cannot go higher than 100!");
+                await Task.Delay(2000);
+                await botMessage_1.DeleteAsync();
+                return;
+            }
+
+            var messages = await Context.Channel.GetMessagesAsync(amount + 1).FlattenAsync();
+            foreach(IMessage message in messages)
+            {
+                await Context.Channel.DeleteMessageAsync(message);
+            }
+            var botMessage_2 = await Context.Channel.SendMessageAsync("Purged " + amount + " messages.");
+            await Task.Delay(2000);
+            await botMessage_2.DeleteAsync();
+        }
+    }
+}
