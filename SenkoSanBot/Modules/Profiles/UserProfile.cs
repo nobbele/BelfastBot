07b6223f97570a27073bdc8598adc480b022d2e5
nobbele <realnobbele@gmail.com>
2019-08-27 22:20:48 +0200

    Fixes crash where users owns too many gacha cards

--- SenkoSanBot/Modules/Profiles/UserProfile.cs
+++ SenkoSanBot/Modules/Profiles/UserProfile.cs
@@ -1,6 +1,8 @@
 ﻿using Discord;
 using Discord.Commands;
 using SenkoSanBot.Services.Database;
+using SenkoSanBot.Services.Pagination;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +12,7 @@ namespace SenkoSanBot.Modules.Profiles
     public class UserProfile : SenkoSanModuleBase
     {
         public JsonDatabaseService Db { get; set; }
+        public PaginatedMessageService PaginatedMessageService { get; set; }
 
         [Command("profile"), Alias("prf")]
         [Summary("Shows details of an users profile")]
@@ -58,24 +61,30 @@ namespace SenkoSanBot.Modules.Profiles
 
             DatabaseUserEntry userData = Db.GetUserEntry(0, target.Id);
 
-            Embed embed = new EmbedBuilder()
+            string characterString = userData.Cards.Count > 0 
+                ? userData.Cards.Select(card => $"► **[{card.Name}](https://www.animecharactersdatabase.com/characters.php?id={card.Id})** x{card.Amount}").NewLineSeperatedString()
+                : "**None**";
+
+            string[] strings = characterString.NCharLimitToClosestDelimeter(512, "\n");
+
+            await PaginatedMessageService.SendPaginatedDataMessageAsync(
+                Context.Channel,
+                strings,
+                (result, index, footer) => GetUserGachaEmbed(target, result, footer)
+            );
+        }
+
+        private Embed GetUserGachaEmbed(IUser target, string gachaString, EmbedFooterBuilder footer) => new EmbedBuilder()
                  .WithColor(0x53DF1D)
                  .WithAuthor(author => {
                      author
                          .WithName($"Profile of {target.Username}")
                          .WithIconUrl($"{target.GetAvatarUrl()}");
                  })
-                 .AddField("Rolled Characters:", $"{(userData.Cards.Count > 0 ? userData.Cards.Select(card => $"► [{userData.Cards.FindIndex(r => r.Id == card.Id)}] **[{card.Name}](https://www.animecharactersdatabase.com/characters.php?id={card.Id})** (x{card.Amount})").NewLineSeperatedString() : "**None**")}")
-                 .WithFooter(footer => {
-                     footer
-                         .WithText($"Requested by {Context.User}")
-                         .WithIconUrl(Context.User.GetAvatarUrl());
-                 })
+                 .AddField("Rolled Characters:", $"{gachaString}")
+                 .WithFooter(footer)
                  .WithThumbnailUrl($"{target.GetAvatarUrl()}")
                  .Build();
-
-            await ReplyAsync(embed: embed);
-        }
 
         [Command("card sell")]
         public async Task SellCardAsync(int index)
@@ -92,6 +101,33 @@ namespace SenkoSanBot.Modules.Profiles
                 return;
             }
             await ReplyAsync("> Couldn't find the specified card index");
+        }
+    }
+    public static class StringExtensionMethods
+    {
+        /// <summary>
+        /// Assumes no string between 2 delimeters is longer than n
+        /// </summary>
+        public static string[] NCharLimitToClosestDelimeter(this string str, int n, string delim)
+        {
+            List<string> list = new List<string>(str.Length / n);
+
+            while (str.Length > n)
+            {
+                int lastIndexToClosestDelim = 0;
+                int indexToClosestDelim = 0;
+                while ((indexToClosestDelim = str.IndexOf(delim, indexToClosestDelim + delim.Length)) < n)
+                {
+                    lastIndexToClosestDelim = indexToClosestDelim;
+                }
+
+                list.Add(str.Substring(0, lastIndexToClosestDelim));
+                str = str.Remove(0, lastIndexToClosestDelim + delim.Length);
+            }
+
+            list.Add(str);
+
+            return list.ToArray();
         }
     }
 }
