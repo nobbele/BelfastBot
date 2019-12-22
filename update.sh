0bc6dfd4071ed61cf82bc3097ca5c6146b73fecb
Nobbele <realnobbele@gmail.com>
2019-12-22 16:08:30 +0100

    fixed update script

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@
 # Make sure process has had time to quit
 sleep 5
 
-find . -maxdepth 1 -not \( -name 'update-old.sh' -o -name '*.json' \)
+find . -maxdepth 1 -not \( -name 'update-old.sh' -o -name '*.xml' \) -exec rm {} \;
 
 cp $1/* .
