31dca18e5d308ad20003a7e1c2c92f79683d6501
Nobbele <realnobbele@gmail.com>
2020-01-03 21:05:28 +0100

    Adds 7k recent

--- SenkoSanBot/Modules/Quaver/QuaverModule.cs
+++ SenkoSanBot/Modules/Quaver/QuaverModule.cs
@@ -97,5 +97,47 @@ namespace SenkoSanBot.Modules.Quaver
                 .Build()
             );
         }
+
+        [Command("quaver recent 7k")]
+        [Summary("Get recent 7k play info by user")]
+        public async Task GetRecent7KAsync([Summary("User to get recent play from")] string name = null)
+        {
+            uint? id = null;
+            if(string.IsNullOrEmpty(name))
+            {
+                id = Db.GetUserEntry(0, Context.User.Id).QuaverId;
+            }
+            else
+            {
+                id = await Client.GetUserIdByNameAsync(name);
+            }
+
+            if(id == null)
+            {
+                await ReplyAsync("Couldn't find any user, please set one or specify in arguments");
+                return;
+            }
+
+            uint userId = id.Value;
+
+            Recent recent = await Client.GetUserRecentAsync(userId, 2);
+            Map map = recent.Map;
+            User user = await Client.GetUserAsync(userId);
+
+            await ReplyAsync(embed: new EmbedBuilder()
+                .WithColor(0xE664A0)
+                .WithAuthor(author => {
+                    author
+                        .WithName($"{user.Username}'s Recent 7K Play")
+                        .WithUrl($"https://quavergame.com/profile/{user.Id}")
+                        .WithIconUrl(user.AvatarUrl);
+                })
+                .AddField("Map", $"**{map.Artist} - {map.Title} [{map.DifficultyName}]**")
+                .AddField("Accuracy", $"{recent.Accuracy.ToString("0.00")}%", true)
+                .AddField("Performance Rating", recent.PerformanceRating.ToString("0.00"), true)
+                .WithThumbnailUrl($"https://quaver.blob.core.windows.net/banners/{map.Id}_banner.jpg")
+                .Build()
+            );
+        }
     }
 }
