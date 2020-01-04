4514a410659088cb99e4666933a57dbbdf6af256
Nobbele <realnobbele@gmail.com>
2020-01-04 16:32:07 +0100

    Added -f to update script

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ rm update-status
 # Make sure process has had time to quit
 sleep 5
 
-find . -maxdepth 1 -not \( -name 'update-old.sh' -o -name '*.xml' \) -exec rm {} \;
+find . -maxdepth 1 -not \( -name 'update-old.sh' -o -name '*.xml' \) -exec rm -f {} \;
 
 cp $1/* .
 
