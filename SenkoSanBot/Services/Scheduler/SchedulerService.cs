043392d2907112bd48d5ce09c6f70778b7096f03
Nobbele <realnobbele@gmail.com>
2020-01-15 17:05:38 +0100

    scheduler stores guids for planned executions

--- SenkoSanBot/Services/Scheduler/SchedulerService.cs
+++ SenkoSanBot/Services/Scheduler/SchedulerService.cs
@@ -1,24 +1,44 @@
 using System;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace SenkoSanBot.Services.Scheduler
 {
     public class SchedulerService
     {
-        public void Add(DateTime time, Action func)
+        private Dictionary<Guid, DateTime> schedules;
+
+        public void Add<T>(DateTime time, T func)
         {
+            Guid guid = Guid.NewGuid();
             TimeSpan delay = time - DateTime.Now;
             if(delay.TotalMilliseconds < 0)
                 return;
-            Task.Delay(delay).ContinueWith(task => func?.Invoke());
+            if(func is Action a)
+                Task.Delay(delay).ContinueWith(task => OnDone(a, guid));
+            if(func is Func<Task> f)
+                Task.Delay(delay).ContinueWith(task => OnDone(f, guid));
+            else
+                throw new ArgumentException("Unsupported function type");
+            schedules.Add(guid, time);
         }
 
-        public void Add(DateTime time, Func<Task, object> func)
+        private void OnDone(Guid guid)
         {
-            TimeSpan delay = time - DateTime.Now;
-            if(delay.TotalMilliseconds < 0)
-                return;
-            Task.Delay(delay).ContinueWith(func);
+            schedules.Remove(guid);
+        }
+
+        private void OnDone(Action func, Guid guid)
+        {
+            func?.Invoke();
+            OnDone(guid);
+        }
+
+        private void OnDone(Func<Task> func, Guid guid)
+        {
+            func?.Invoke();
+            OnDone(guid);
         }
     }
 }
